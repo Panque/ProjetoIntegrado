ec3c0b8b796bf2414ac683a0d077ed172af56949
Renan Rossignatti de França <renanrfranca@gmail.com>
2017-06-17 12:10:51 -0300

    Criado bean de Actor e ActorMovie

---
+++ src/java/model/Actor.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author renan
+ */
+public class Actor {
+	private int id;
+	private String name;
+	private char sex;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	
+
+}
