bb9aab8332c5945e8a328f315851c08efb34d474
Renan Rossignatti de França <renanrfranca@gmail.com>
2017-06-17 08:32:09 -0300

    Criado bean de movie

---
+++ src/java/model/Movie.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author renan
+ */
+public class Movie {
+	private int id;
+	private String title;
+	private String year;
+	private String genres;	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getGenres() {
+		return genres;
+	}
+
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+}
