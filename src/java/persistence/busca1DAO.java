d9b650d720dfcfc2125dfeb2f940accb852b4102
Renan Rossignatti de França <renanrfranca@gmail.com>
2017-06-22 09:18:20 -0300

    Versão de teste do busca1DAO concluída

--- src/java/persistence/busca1DAO.java
+++ src/java/persistence/busca1DAO.java
@@ -6,6 +6,8 @@
 package persistence;
 
 import java.sql.*;
+import java.util.ArrayList;
+import model.*;
 /**
  *
  * @author renan
@@ -15,5 +17,57 @@ public class busca1DAO {
 	
 	public busca1DAO() throws DAOException {
 		this.conn = ConnectionFactory.getConnection();
+	}
+	
+	public ArrayList<Movie> busca(
+		ArrayList<String> atores,
+		ArrayList<String> diretores,
+		ArrayList<String> generos
+	) throws SQLException {	
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		ArrayList<Movie> resultado = new ArrayList<>();
+		
+//		String SQL = "SELECT DISTINCT" +
+//			"m.movieid AS movie_ID," +
+//			"m.title AS titulo, " +
+//			"m.mvyear AS ano" +
+//		"FROM " +
+//			"movies m, " +
+//			"((actormovie INNER JOIN actor " +
+//				"ON(actormovie.actor_id = actor.actor_id)) as am " +
+//				"INNER JOIN (genero INNER JOIN generomovie " +
+//					"ON (genero.genero_id = generomovie.genero_id)) as gm " +
+//				"ON (gm.movieid = am.movieid)) " +
+//		"WHERE " +
+//			"am.movieId = m.movieId AND m.movieId = gm.movieId " +
+//			"AND (lower(am.name) LIKE any(array[<lower('ator%')>]) " +
+//			"OR lower(am.as_character) LIKE any (array[<lower('personagem%')>]) " +
+//			"OR lower(gm.descricao) LIKE any (array[<lower('genero%')>]))";
+//		return null;
+
+		String SQL = "SELECT m.*, a.* "
+			+ "FROM actormovie am "
+				+ "INNER JOIN actor a ON am.actor_id = a.actor_id "
+				+ "INNER JOIN movies m ON am.movieid = m.movieid "
+			+ "WHERE a.\"name\" = ?";
+		
+		try {
+			ps = conn.prepareStatement(SQL);
+			ps.setString(1, atores.get(0));
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()){
+				Movie m = new Movie(rs.getInt(1), rs.getString(2), rs.getString(3));
+				resultado.add(m);
+			}
+			
+		} catch (SQLException e) {
+			
+		}
+	
+		return resultado;
 	}
 }
